65561038244a8655b7dae9e21c82ad44787e6f59
THISJowi <thsjowi@gmail.com>
2026-04-11 11:17:26 +0200

    remove: auth service

--- auth/src/main/java/com/thisjowi/auth/entity/Account.java
+++
@@ -1,6 +0,0 @@
-package com.thisjowi.auth.entity;
-
-public enum Account {
-    Business,
-    Community,
-}
--- auth/src/main/java/com/thisjowi/auth/entity/Deployment.java
+++
@@ -1,7 +0,0 @@
-package com.thisjowi.auth.entity;
-
-public enum Deployment {
-    SelfHosted,
-    Cloud,
-    
-}
